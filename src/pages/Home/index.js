1ba71d4744230dffebba5aaf0a8451b8b5c903b4
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-08-10 12:08:13 -0300

    retornando os dados com o método get da api através de uma função assíncrona

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,28 @@
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+
+// movie/now_playing?api_key=fd5323a78d165650b73a59a1fe5cbc9c&language=pt-BR
+
 function Home() {
+    const [filmes, setFilmes] = useState([]);
+
+    useEffect(() => {
+        async function loadFilmes() {
+            const response = await api.get("movie/now_playing", {
+                params:{
+                    api_key: "fd5323a78d165650b73a59a1fe5cbc9c",
+                    language: "pt-BR",
+                    page: 1
+                }
+            });
+
+            console.log(response.data.results);
+        }
+
+        loadFilmes();
+
+    }, [])
+
     return(
         <div>
             <h1>Seja bem-vindo à Home!</h1>
