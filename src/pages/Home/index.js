1da52f5eb30391b8dffa9bbccaacbf965d68c7d2
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-08-11 11:33:46 -0300

    Criando a listagem dos filmes em cartaz na página home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import api from "../../services/api";
+import './style.css';
 
 // movie/now_playing?api_key=fd5323a78d165650b73a59a1fe5cbc9c&language=pt-BR
 
@@ -16,7 +18,8 @@ function Home() {
                 }
             });
 
-            console.log(response.data.results);
+            // console.log(response.data.results.slice(0, 10));
+            setFilmes(response.data.results);
         }
 
         loadFilmes();
@@ -24,8 +27,18 @@ function Home() {
     }, [])
 
     return(
-        <div>
-            <h1>Seja bem-vindo à Home!</h1>
+        <div className="container">
+            <div className="lista-filmes">
+                {filmes.map((filme) => {
+                    return(
+                        <article key={filme.id}>
+                            <strong>{filme.title}</strong>
+                            <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+                            <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                        </article>
+                    )
+                })}
+            </div>
         </div>
     )
 }
