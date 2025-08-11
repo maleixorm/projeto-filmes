abbf91608a0735fb4d2774cab6b16e7382221d37
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-08-11 11:58:17 -0300

    Adicionando a página de erro às rotas da aplicação

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 import Home from './pages/Home';
 import Filme from "./pages/Filme";
-import Header from "./components/Header";
+import Erro from "./pages/Erro";
 
 function RoutesApp() {
     return(
@@ -10,6 +11,9 @@ function RoutesApp() {
             <Routes>
                 <Route path="/" element={ <Home/> } />
                 <Route path="/filme/:id" element={ <Filme/> } />
+
+
+                <Route path="*" element={ <Erro/> } />
             </Routes>
         </BrowserRouter>
     )
