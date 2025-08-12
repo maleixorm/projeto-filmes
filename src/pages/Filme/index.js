cd3e0aaacb859ef41d2d5c16129cf78d1dc46723
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-08-12 11:04:21 -0300

    Criando a página de detalhe do filme

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,7 +1,51 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import api from '../../services/api';
+import './style.css';
+
 function Filme() {
+    const { id } = useParams();
+    const [filme, setFilme] = useState({});
+    const [loading, setLoading] = useState(true);
+    
+    useEffect(() => {
+        async function loadFilme() {
+            await api.get(`/movie/${id}`, {
+                params:{
+                    api_key: "fd5323a78d165650b73a59a1fe5cbc9c",
+                    language: "pt-BR",
+                }
+            })
+            .then((response) => {
+                setFilme(response.data);
+                setLoading(false);
+            })
+            .catch(() => {
+                console.log("Filme não encontrado!");
+            })
+        }
+        loadFilme();
+
+        return () => {
+            console.log("Componente foi desmontado!");
+        }
+    }, [])
+
+    if (loading) {
+        return(
+            <div className="loading">
+                <h2>Carregando detalhes do filme...</h2>
+            </div>
+        )
+    }
+    
     return(
-        <div>
-            <h1>Bem-vindo à página Detalhes do Filme</h1>
+        <div className="filme-info">
+            <h1>{filme.title}</h1>
+            <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
+            <h3>Sinopse:</h3>
+            <span>{filme.overview}</span>
+            <strong>Avaliação: {filme.vote_average} / 10</strong>
         </div>
     )
 }
