1a158682bc213d90ef8d3645966f570390e4deb3
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-08-18 11:30:09 -0300

    Criando a página de filmes favoritos

---
+++ src/pages/Favoritos/index.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+import './style.css';
+import { Link } from 'react-router-dom';
+
+function Favoritos() {
+
+    const [filmes, setFilmes] = useState([]);
+
+    useEffect(() => {
+        const minhaLista = localStorage.getItem("@primeflix");
+        setFilmes(JSON.parse(minhaLista) || []);
+    }, [])
+
+    return(
+        <div className='meus-filmes'>
+            <h1>Meus Filmes Favoritos</h1>
+            <ul>
+                {filmes.map((item) => {
+                    return(
+                        <li key={item.id}>
+                            <span>{item.title}</span>
+                            <div>
+                                <Link to={`/filme/${item.id}`}>Ver detalehs</Link>
+                                <button>Excluir</button>
+                            </div>
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
+    )
+}
+
+export default Favoritos;
