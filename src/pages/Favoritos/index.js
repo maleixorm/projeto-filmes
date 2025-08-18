fd6dabd0dc7acff1f97b1513e5c4c2b52ec4c6aa
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-08-18 11:48:53 -0300

    Criando a função para excluir os filmes da lista de favoritos

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -11,6 +11,14 @@ function Favoritos() {
         setFilmes(JSON.parse(minhaLista) || []);
     }, [])
 
+    function excluirFilme(id) {
+        let filtroFilmes = filmes.filter( (item) => {
+            return (item.id !== id);
+        });
+        setFilmes(filtroFilmes);
+        localStorage.setItem("@primeflix", JSON.stringify(filtroFilmes));
+    }
+
     return(
         <div className='meus-filmes'>
             <h1>Meus Filmes Favoritos</h1>
@@ -21,7 +29,7 @@ function Favoritos() {
                             <span>{item.title}</span>
                             <div>
                                 <Link to={`/filme/${item.id}`}>Ver detalhes</Link>
-                                <button>Excluir</button>
+                                <button onClick={() => excluirFilme(item.id)}>Excluir</button>
                             </div>
                         </li>
                     )
